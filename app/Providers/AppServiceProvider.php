598e19c1a68ee92d3c36a4a1c381c9b0d6254e38
asyncdeveloper <contact@asyncdeveloper.com>
2023-11-11 01:31:27 +0100

    bind contract to utils

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Utilities\Contracts\ElasticsearchHelperInterface;
+use App\Utilities\Contracts\RedisHelperInterface;
+use App\Utilities\Services\ElasticsearchHelper;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ElasticsearchHelperInterface::class, ElasticsearchHelper::class);
+        $this->app->bind(RedisHelperInterface::class, RedisHelper::class);
     }
 
     /**
